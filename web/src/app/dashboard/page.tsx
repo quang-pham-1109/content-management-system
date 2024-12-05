bf6fcb1311c774db4cfd703cf2adb4af730e2133
quang-pham-1109 <phvuquang@gmail.com>
2024-12-05 21:34:52 +0700

    chore: add comments

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -18,6 +18,7 @@ const DashboardPage = () => {
       <AppSidebar />
 
       <SidebarInset>
+        {/* Header Section */}
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
@@ -29,6 +30,8 @@ const DashboardPage = () => {
             </BreadcrumbList>
           </Breadcrumb>
         </header>
+
+        {/* Content Section */}
         <div className="flex flex-1 flex-col gap-4 p-4">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
